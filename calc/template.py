86506ac6e4eddaa6a5f99e205b246fe7bb190101
kueyeon0429 <20203065@kookmin.ac.kr>
2020-06-29 14:22:58 +0900

    issues #2

---
+++ calc/template.py
@@ -0,0 +1,14 @@
+html = b"""
+<html>
+    <body>
+        <form method="get" action="">
+            a = <input type="number" name="a"><br>
+            b = <input type="number" name="b"><br>
+            <input type="submit">
+        </form>
+        sum : %(sum)s</br>
+        product : %(product)s</br>
+    </body>
+</html>
+"""
+
